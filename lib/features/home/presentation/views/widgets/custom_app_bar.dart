448d19711df4920beb7b9c17373d476a28b420b1
raufzer <a_zerkhef@estin.dz>
2024-10-15 19:26:11 +0100

    chore: add custom app bar

---
+++ lib/features/home/presentation/views/widgets/custom_app_bar.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../../../../../core/utils/assets.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 40),
+      child: Row(
+        children: [
+          Image.asset(AssetsData.gramazLogo),
+          const Spacer(),
+          GestureDetector(
+            onTap: () {},
+            child: SvgPicture.asset(
+              AssetsData.shoppingCartIcon,
+              height: 20.r,
+              width: 20.r,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:gramaz_app/features/home/presentation/views/widgets/custom_app_bar.dart';
+
+class HomeViewBody extends StatelessWidget {
+  const HomeViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CustomAppBar(),
+      ],
+    );
+  }
+}
