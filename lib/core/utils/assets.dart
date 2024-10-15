a0024fa25096851e0fd8ad49ee6e739c238d1552
raufzer <a_zerkhef@estin.dz>
2024-10-15 20:44:15 +0100

    chore: adjust assets

--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -1,4 +1,5 @@
 class AssetsData {
   static const gramazLogo = 'assets/images/gramaz_logo.png';
   static const shoppingCartIcon = 'assets/icons/shopping_cart_icon.svg';
+  static const testImage = 'assets/images/test_image.png';
 }
