caeac1dd416c3aac66478d76f2884843b323a580
raufzer <a_zerkhef@estin.dz>
2024-10-15 17:57:08 +0100

    chore(ui): craft splash screen & adjust theme

---
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:gramaz_app/core/utils/assets.dart';
+
+class SplashViewBody extends StatelessWidget {
+  const SplashViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Image.asset(AssetsData.gramazLogo),
+      ],
+    );
+  }
+}
