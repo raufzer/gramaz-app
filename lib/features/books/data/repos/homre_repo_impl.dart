0c4509794edd1b19ea2ff73c83111d48f58d569f
raufzer <a_zerkhef@estin.dz>
2024-10-16 21:05:09 +0100

    chore: implement fetch newest books

--- lib/features/books/data/repos/homre_repo_impl.dart
+++ lib/features/books/data/repos/homre_repo_impl.dart
@@ -1,18 +1,28 @@
 import 'package:dartz/dartz.dart';
 import 'package:gramaz_app/core/errors/failures.dart';
+import 'package:gramaz_app/core/utils/api_service.dart';
 import 'package:gramaz_app/features/books/data/models/book_model/book_model.dart';
 import 'package:gramaz_app/features/books/data/repos/home_repo.dart';
 
 class HomreRepoImpl implements HomeRepo {
+  final ApiService apiService;
+
+  HomreRepoImpl(this.apiService);
   @override
-  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() {
-   
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchBestNewestBooks() async {
+    try {
+      var data = await apiService.get(
+          endPoint: 'volumes?q=subject:programming&Filtering=free-ebooks');
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } catch (e) {
+      return left(ServerFailure());
+    }
   }
 
   @override
-  Future<Either<Failure, List<BookModel>>> fetchFeatureBooks() {
-
-    throw UnimplementedError();
-  }
+  Future<Either<Failure, List<BookModel>>> fetchFeatureBooks() {}
 }
