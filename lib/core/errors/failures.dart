54e8e3d5a7152a8927fa48338fe96ecdaaafb986
raufzer <a_zerkhef@estin.dz>
2024-10-16 21:16:51 +0100

    chore: home repo adjust

--- lib/core/errors/failures.dart
+++ lib/core/errors/failures.dart
@@ -1,3 +1,53 @@
-abstract class Failure {}
+import 'package:dio/dio.dart';
 
-class ServerFailure extends Failure {}
+abstract class Failure {
+  final String errorMessage;
+
+  Failure(this.errorMessage);
+}
+
+class ServerFailure extends Failure {
+  ServerFailure(super.errorMessage);
+
+
+  factory ServerFailure.fromDioError(DioException dioError) {
+    switch (dioError.type) {
+      case DioExceptionType.connectionTimeout:
+        return ServerFailure("Connection timeout with the server.");
+      case DioExceptionType.sendTimeout:
+        return ServerFailure("Send timeout in connection with the server.");
+      case DioExceptionType.receiveTimeout:
+        return ServerFailure("Receive timeout in connection with the server.");
+      case DioExceptionType.badCertificate:
+        return ServerFailure("Bad certificate detected.");
+      case DioExceptionType.badResponse:
+        return ServerFailure(_handleBadResponse(dioError.response?.statusCode));
+      case DioExceptionType.cancel:
+        return ServerFailure("Request to the server was cancelled.");
+      case DioExceptionType.connectionError:
+        return ServerFailure("Connection error occurred.");
+      case DioExceptionType.unknown:
+        return ServerFailure("An unknown error occurred.");
+      }
+  }
+
+  static String _handleBadResponse(int? statusCode) {
+    if (statusCode != null) {
+      switch (statusCode) {
+        case 400:
+          return "Bad request.";
+        case 401:
+          return "Unauthorized access.";
+        case 403:
+          return "Forbidden access.";
+        case 404:
+          return "Resource not found.";
+        case 500:
+          return "Internal server error.";
+        default:
+          return "Received an invalid response: $statusCode.";
+      }
+    }
+    return "Received an invalid response with no status code.";
+  }
+}
--- lib/features/books/data/repos/homre_repo_impl.dart
+++ lib/features/books/data/repos/homre_repo_impl.dart
@@ -1,4 +1,5 @@
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 import 'package:gramaz_app/core/errors/failures.dart';
 import 'package:gramaz_app/core/utils/api_service.dart';
 import 'package:gramaz_app/features/books/data/models/book_model/book_model.dart';
@@ -8,6 +9,7 @@ class HomreRepoImpl implements HomeRepo {
   final ApiService apiService;
 
   HomreRepoImpl(this.apiService);
+
   @override
   Future<Either<Failure, List<BookModel>>> fetchBestNewestBooks() async {
     try {
@@ -19,10 +21,30 @@ class HomreRepoImpl implements HomeRepo {
       }
       return right(books);
     } catch (e) {
-      return left(ServerFailure());
+      if (e is DioException) {
+        return left(ServerFailure.fromDioError(e));
+      } else {
+        return left(ServerFailure("An unexpected error occurred."));
+      }
     }
   }
 
   @override
-  Future<Either<Failure, List<BookModel>>> fetchFeatureBooks() {}
+  Future<Either<Failure, List<BookModel>>> fetchFeatureBooks() async {
+    try {
+      var data = await apiService.get(
+          endPoint: 'volumes?q=featured&Filtering=free-ebooks');
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.fromDioError(e));
+      } else {
+        return left(ServerFailure("An unexpected error occurred."));
+      }
+    }
+  }
 }
