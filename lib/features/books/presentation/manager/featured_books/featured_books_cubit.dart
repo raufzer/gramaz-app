f7ed582d63f1440f5f8eb75e8f313f0f6f475ef3
raufzer <a_zerkhef@estin.dz>
2024-10-16 21:55:07 +0100

    chore: create featured books cubit

---
+++ lib/features/books/presentation/manager/featured_books/featured_books_cubit.dart
@@ -0,0 +1,9 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:gramaz_app/features/books/data/models/book_model/book_model.dart';
+
+part 'featured_books_state.dart';
+
+class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
+  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+}
