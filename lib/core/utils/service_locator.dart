df184a5b395c4f75876e636552f9313a0e886cec
raufzer <a_zerkhef@estin.dz>
2024-10-17 02:19:17 +0100

    chore: service locater set

---
+++ lib/core/utils/service_locator.dart
@@ -0,0 +1,13 @@
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+import 'package:gramaz_app/core/utils/api_service.dart';
+import 'package:gramaz_app/features/books/data/repos/homre_repo_impl.dart';
+
+final getIt = GetIt.instance;
+
+void setup() {
+  getIt.registerSingleton<ApiService>(ApiService(Dio()));
+  getIt.registerSingleton<HomreRepoImpl>(HomreRepoImpl(
+    getIt.get<ApiService>(),
+  ));
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter/material.dart';
 import 'package:gramaz_app/core/utils/app_routers.dart';
 import 'package:gramaz_app/core/utils/colors.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:gramaz_app/core/utils/service_locator.dart';
+import 'package:gramaz_app/features/books/data/repos/homre_repo_impl.dart';
+import 'features/books/presentation/manager/featured_books_cubit/featured_books_cubit.dart';
+import 'features/books/presentation/manager/newest_books_cubit/newest_books_cubit.dart';
 
 void main() {
+  setup();
   runApp(const GramazApp());
 }
 
@@ -18,19 +24,30 @@ class GramazApp extends StatelessWidget {
       minTextAdapt: true,
       splitScreenMode: true,
       builder: (_, __) {
-        return MaterialApp.router(
-          routerConfig: AppRouters.router,
-          theme: ThemeData.dark().copyWith(
-            scaffoldBackgroundColor: kPrimaryColor,
-            textTheme: GoogleFonts.playfairDisplayTextTheme(
-              ThemeData.dark().textTheme.apply(
-                    bodyColor: kTertiaryColor,
-                    displayColor: kTertiaryColor,
-                  ),
+        return MultiBlocProvider(
+          providers: [
+            BlocProvider(
+              create: (context) =>
+                  FeaturedBooksCubit(getIt.get<HomreRepoImpl>()),
             ),
+            BlocProvider(
+              create: (context) => NewestBooksCubit(getIt.get<HomreRepoImpl>()),
+            ),
+          ],
+          child: MaterialApp.router(
+            routerConfig: AppRouters.router,
+            theme: ThemeData.dark().copyWith(
+              scaffoldBackgroundColor: kPrimaryColor,
+              textTheme: GoogleFonts.playfairDisplayTextTheme(
+                ThemeData.dark().textTheme.apply(
+                      bodyColor: kTertiaryColor,
+                      displayColor: kTertiaryColor,
+                    ),
+              ),
+            ),
+            debugShowCheckedModeBanner: false,
+            title: 'Gramaz',
           ),
-          debugShowCheckedModeBanner: false,
-          title: 'Gramaz',
         );
       },
     );
